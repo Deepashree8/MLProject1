8337a52add33b2583caefb35dab1415364810522
deepashree <deepashreekumar813@gmail.com>
2025-09-20 23:59:34 +0530

    prediction pipleine added

--- src/components/data_injection.py
+++ src/components/data_injection.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 #input where need to see train data 
 #output folder 
@@ -51,6 +53,9 @@ if __name__=="__main__":
     
     data_transformation=DataTransformation()
     train_arr,test_arr,pre_obj_path=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modelTrainer=ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr,test_arr))
     
         
     
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,9 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -12,5 +14,42 @@ def save_object(file_path,obj):
         
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
+    try:
+        report={}
+        for model_name,model in models.items():
+            para=param(model_name)
+            
+            #cross validation=cv
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            
+            #model.fit(x_train,y_train) #Train the model
+            
+            
+            y_train_pred=model.predict(x_train)
+            y_test_pred=model.predict(x_test)
+            ##between true values y and predicted y
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+            
+            report[model_name]=test_model_score
+        return report 
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+            
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
